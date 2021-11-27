eb34e8b3ea0a9f2e2c55293fc56bf2d681f94dc8
Simon Xu <13837836+simjxu@users.noreply.github.com>
2021-11-26 21:28:51 -0800

    add files

---
+++ ios_shortcuts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_shortcuts
+//
+//  Created by Simon Xu on 11/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
