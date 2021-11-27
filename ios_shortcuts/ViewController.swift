71fe99c03227055041c00316b2f2a1656e957f61
Simon Xu <13837836+simjxu@users.noreply.github.com>
2021-11-26 21:49:59 -0800

    Added Label

--- ios_shortcuts/ViewController.swift
+++ ios_shortcuts/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Label1.text = "Simon's App"
     }
 
 
