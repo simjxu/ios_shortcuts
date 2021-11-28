5a16f4cd3e049b1521684b9bbacb7e79b609e274
Simon Xu <13837836+simjxu@users.noreply.github.com>
2021-11-27 21:10:25 -0800

    Added flashlight functionality

--- ios_shortcuts/ViewController.swift
+++ ios_shortcuts/ViewController.swift
@@ -8,14 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Setting the light on variable for controlling background color
+    var lightOn = true
+    
     @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var myButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         Label1.text = "Simon's App"
+        updateUI()
     }
-
-
+    
+    fileprivate func updateUI() {
+        if lightOn{
+            view.backgroundColor = .white
+            myButton.setTitleColor(.red, for: .normal)
+            myButton.setTitle("Off", for: .normal)
+        } else {
+            view.backgroundColor = .black
+            myButton.setTitleColor(.green, for: .normal)
+            myButton.setTitle("On", for:. normal)
+            
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
+    
+    
 }
 
